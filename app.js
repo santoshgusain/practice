d7648f6e10cf68e0c5b6f7532b14bd4783f70237
santosh gusain <santosh1998gusain@gmail.com>
2021-09-20 22:18:23 +0530

    added new lines

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 console.log("welcome to this tutorial");
 console.log("welcome to this tutorial");
 console.log("modified this line number 4");
+console.log("modified this line number 5");
+console.log("modified this line number 6");
+console.log("modified this line number 7");
+console.log("modified this line number 8");
