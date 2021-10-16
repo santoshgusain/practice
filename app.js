34e5b9ed1d7458b14c62d0bdb9807f4b535ed20c
santosh gusain <santosh1998gusain@gmail.com>
2021-10-16 22:05:45 +0530

    line 8 is modified

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ console.log("This is the new line which i have added");
 
 console.log("modified this line number 112");
 console.log("modified this line number 6");
-console.log("modified this line number 7");
+console.log("modified this line number 78");
 console.log("modified this line number 8");
 console.log("This line is added by me at line number 3");
 console.log("This line is added by me at line number 31");
